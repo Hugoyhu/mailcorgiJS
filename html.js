7db97ff7c13ad786b13f758396e5dd86a9db2bcf
Hugo Hu <69009840+Hugoyhu@users.noreply.github.com>
2022-02-26 18:21:50 -0500

    Delete html.js

--- html.js
+++
@@ -1,7 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-
-const orderCode = params.get("code");
-
-demo = document.getElementById("status").innerHTML = orderCode;
-
-console.log(orderCode);
